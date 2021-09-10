4c13a743633d57d426dc55dfe30426efcc48a6f8
EnesKy <eneskamilyilmaz@gmail.com>
2021-09-10 11:21:46 +0300

    Firebase Crashlytics added.

--- app/src/main/java/com/enesky/evimiss/App.kt
+++ app/src/main/java/com/enesky/evimiss/App.kt
@@ -1,11 +1,10 @@
 package com.enesky.evimiss
 
 import android.app.Application
+import android.os.Build
 import com.jakewharton.threetenabp.AndroidThreeTen
 import com.google.firebase.analytics.FirebaseAnalytics
-
-
-
+import com.google.firebase.crashlytics.FirebaseCrashlytics
 
 /**
  * Created by Enes Kamil YILMAZ on 05/09/2021
@@ -14,11 +13,18 @@ import com.google.firebase.analytics.FirebaseAnalytics
 class App: Application() {
 
     private var mFirebaseAnalytics: FirebaseAnalytics? = null
+    private var mFirebaseCrashlytics: FirebaseCrashlytics? = null
 
     override fun onCreate() {
         super.onCreate()
         AndroidThreeTen.init(this)
+
         mFirebaseAnalytics = FirebaseAnalytics.getInstance(this)
+        mFirebaseCrashlytics = FirebaseCrashlytics.getInstance()
+
+        val tempUserId = Build.BRAND + " " + Build.DEVICE
+        mFirebaseAnalytics?.setUserId(tempUserId)
+        mFirebaseCrashlytics?.setUserId(tempUserId)
     }
 
 }
