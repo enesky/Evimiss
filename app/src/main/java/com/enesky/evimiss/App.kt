3b8acae345077df2a544b46a241363dfd6163fef
EnesKy <eneskamilyilmaz@gmail.com>
2021-09-10 10:41:05 +0300

    Firebase Analytics added.

--- app/src/main/java/com/enesky/evimiss/App.kt
+++ app/src/main/java/com/enesky/evimiss/App.kt
@@ -2,6 +2,10 @@ package com.enesky.evimiss
 
 import android.app.Application
 import com.jakewharton.threetenabp.AndroidThreeTen
+import com.google.firebase.analytics.FirebaseAnalytics
+
+
+
 
 /**
  * Created by Enes Kamil YILMAZ on 05/09/2021
@@ -9,9 +13,12 @@ import com.jakewharton.threetenabp.AndroidThreeTen
 
 class App: Application() {
 
+    private var mFirebaseAnalytics: FirebaseAnalytics? = null
+
     override fun onCreate() {
         super.onCreate()
         AndroidThreeTen.init(this)
+        mFirebaseAnalytics = FirebaseAnalytics.getInstance(this)
     }
 
 }
